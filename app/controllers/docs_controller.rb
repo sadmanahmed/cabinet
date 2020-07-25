1457a513a796fc8f7f9221c797d38018a1ffc56e
sadmanahmed <abirsadmanahmed177@gmail.com>
2020-07-26 01:34:11 +0600

    Updated the index action and route file for that action

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,10 @@ class DocsController < ApplicationController
 before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 def index
-  @docs = Doc.all.order("created_at DESC")
+
+  #@docs = Doc.all.order("created_at DESC")
+  #This will enable to see others doc also
+  @docs = Doc.where(user_id: current_user)
 end
 
 def show
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+  authenticated :user do
+    root "docs#index",as: "authenticated_root"
+  end
+
   root 'welcome#index'
 
   resources :docs
